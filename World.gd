43696cff6b9ba5500e79919cc182a7267a6335d4
Tyler Polito <0613.tyler@gmail.com>
2021-04-24 10:08:17 -0500

    Create main scene + add default background

---
+++ World.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	VisualServer.set_default_clear_color(Color.blue)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
