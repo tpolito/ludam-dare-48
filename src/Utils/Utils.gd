e1818decb4464b2c881454174d3598edf94e2927
Tyler Polito <0613.tyler@gmail.com>
2021-04-24 12:36:25 -0500

    Add utils file + add to autoload

---
+++ src/Utils/Utils.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Used to easily instance scenes at a given position
+func instance_scene_on_main(scene, position):
+	var main = get_tree().current_scene()
+	var instance = scene.instance()
+	
+	main.add_child(instance)
+	instance.global_position = position
+	
+	return instance
+
+func choose(array):
+	array.shuffle()
+	return array.front()
