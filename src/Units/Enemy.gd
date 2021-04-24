4683f1a3e7e11dbc10a8b62e54e5eb824c3c49d2
Thomson Tran <thomson.tran@gmail.com>
2021-04-24 17:23:16 -0400

    stamina, moved colision logic from enemy to player

--- src/Units/Enemy.gd
+++ src/Units/Enemy.gd
@@ -4,10 +4,4 @@ signal eat
 func _ready():
 	pass
 
-func _on_CollisionDetector_body_entered(body):
-	print(size)
-	if body.size >= self.size:
-		body.increase_size()
-		queue_free()
-	else:
-		body.die()
+
